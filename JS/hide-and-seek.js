1ecf66ae84339af6b6359ba0858ba74733135b38
Rashed Ahmed Rahat <37572137+rashedrahat@users.noreply.github.com>
2019-03-24 12:51:19 +0600

    Add files via upload

---
+++ JS/hide-and-seek.js
@@ -0,0 +1,47 @@
+function expNewIn() {
+	document.getElementById("NewIn").style.display = "block";
+	document.getElementById("closeNewIn").style.display = "block";
+	document.getElementById("openNewIn").style.display = "none";
+}
+
+function hideNewIn() {
+	document.getElementById("NewIn").style.display = "none";
+	document.getElementById("closeNewIn").style.display = "none";
+	document.getElementById("openNewIn").style.display = "block";
+}
+
+function expWomen() {
+	document.getElementById("Women").style.display = "block";
+	document.getElementById("closeWomen").style.display = "block";
+	document.getElementById("openWomen").style.display = "none";
+}
+
+function hideWomen() {
+	document.getElementById("Women").style.display = "none";
+	document.getElementById("closeWomen").style.display = "none";
+	document.getElementById("openWomen").style.display = "block";
+}
+
+function expMen() {
+	document.getElementById("Men").style.display = "block";
+	document.getElementById("closeMen").style.display = "block";
+	document.getElementById("openMen").style.display = "none";
+}
+
+function hideMen() {
+	document.getElementById("Men").style.display = "none";
+	document.getElementById("closeMen").style.display = "none";
+	document.getElementById("openMen").style.display = "block";
+}
+
+function expKids() {
+	document.getElementById("Kids").style.display = "block";
+	document.getElementById("closeKids").style.display = "block";
+	document.getElementById("openKids").style.display = "none";
+}
+
+function hideKids() {
+	document.getElementById("Kids").style.display = "none";
+	document.getElementById("closeKids").style.display = "none";
+	document.getElementById("openKids").style.display = "block";
+}
